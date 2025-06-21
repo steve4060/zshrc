4ad92431f8623dcd3520b8d12c402585653dfbd0
steve4060 <steve.4060.ri@gmail.com>
2025-06-21 17:43:58 +0200

    add alpha to nvim

--- config-sync/.config/nvim/lua/plugins/treesitter.lua
+++ config-sync/.config/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rust", "java" },
+            auto_install = true,
             highlight = { enable = true},
             indent = { enable = true },
         })
